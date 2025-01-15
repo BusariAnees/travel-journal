e5fe2d0256ded157b4bd0283a7fe5ac248b8c2b1
Anees <ayomideanees2003@icloud.com>
2025-01-15 17:06:13 +0100

    working on the main content

---
+++ src/components/Entry.jsx
@@ -0,0 +1,71 @@
+export default function Header() {
+  return (
+    <>
+      <main>
+        <section>
+          <div className="first-div">
+            <img
+              className="places-img"
+              src="./mount-fuji.webp"
+              alt="mount fuji"
+            />
+          </div>
+          <div className="second-div">
+            <ul>
+              <span className="span">
+                <img
+                  className="location-dot"
+                  src="./location-dot-solid.svg"
+                  alt=""
+                />
+                <p> JAPAN</p>
+                <a href="https://maps.app.goo.gl/9sHrxQ5sUTW8yi3S7">
+                  View on Google Maps
+                </a>
+              </span>
+              <h1>Mount Fuji</h1>
+              <p className="date-p">20 jan 2024 - 30 jan 2024</p>
+              <p className="description-p">
+                Climbing Mount Fuji at dawn was a surreal experience—the crisp
+                mountain air filling my lungs as I ascended through the mist.
+                The summit offered a breathtaking view of the vast landscape
+                below, bathed in the golden light of sunrise.
+              </p>
+            </ul>
+          </div>
+        </section>
+        <section>
+          <div className="first-div">
+            <img
+              className="places-img"
+              src="./great-wall.webp"
+              alt="wall of china"
+            />
+          </div>
+          <div className="second-div">
+            <ul>
+              <span className="span">
+                <img
+                  className="location-dot"
+                  src="./location-dot-solid.svg"
+                  alt=""
+                />
+                <p> CHINA</p>
+                <a href="https://maps.app.goo.gl/KF1coPb1yyednBRv5">
+                  View on Google Maps
+                </a>
+              </span>
+              <h1>Great wall of China</h1>
+              <p className="date-p">12 August 2024 - 20 August 2024</p>
+              <p className="description-p">
+                Walking along the Great Wall of China was like stepping into
+                history, the ancient stone path winding through mist-covered
+                mountains. Each step felt like a connection to the past.
+              </p>
+            </ul>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 export default function Header() {
     return(
         <>
-        
+        <header>
+        <img className="header-image" src="./globe-solid.svg" alt="globe" />
+        <h1>my travel journal</h1>
+        </header>
         </>
     )
 }
